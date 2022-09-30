f60c7e57b6f1bf75b8b88ba0bf6c7bb4d4eee608
Sam Espe <sam.espe.sci@gmail.com>
2022-09-30 15:22:11 -0500

    Got dropdown working and passing values to the otuput functions.

--- data/static/js/app.js
+++ data/static/js/app.js
@@ -7,17 +7,41 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 // let objectValue  = "";
 let selector = d3.select("#selDataset");
 
+// Declare functions
+
+// Handle Changes - Update graphs
+function optionChanged(sampleID) {
+    console.log(`value is: ${sampleID}`);
+    makeBarGraph(sampleID);
+    makeBubbleChart(sampleID);
+    showMetadata(sampleID);
+};
+
+// Make Bar Graph Function
+function makeBarGraph(sampleID){
+  console.log(`Make bar graph of sampleID ${sampleID}`)      
+};
+// Make Bubble Chart Function
+function makeBubbleChart(sampleID){
+    console.log(`Make bubble chart of sampleID ${sampleID}`)  
+};
+// Show Metadata Function
+function showMetadata(sampleID){
+    console.log(`Display metadata of sampleID ${sampleID}`)  
+};
+
+//Create the dashboard
 d3.json(url).then(function(data){
     // Create an HTML object line for each ID number, create <object> tags and add them to HTML with D3
     for (let j = 0; j < data.names.length; j++){
         let objectValue = data.names[j]
-        console.log(`objectValue = ${objectValue}`);
+        //console.log(`objectValue = ${objectValue}`);
         selector.append("option").property("value", objectValue).text(objectValue);
     }
+    
+    // Find the value of the dropdown
+        let sampleID = selector.property("value");
+        console.log(`SampleID is: ${sampleID}`);
+
+       
 });
-
-// Create horizontal bar chart
-
-// Create bubble chart
-
-// Display metadata/demographic info
